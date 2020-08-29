e6d2feeb135e458649eec2e19adec286499db29c
vuonga1103 <vuonga1103@yahoo.com>
2020-08-29 17:23:21 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css'
+
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent 
+      handleClick={ e => (e.target.style.color = e.target.style.color ? null : 'red') }
+    />
   </div>,
   document.getElementById('root')
 );
